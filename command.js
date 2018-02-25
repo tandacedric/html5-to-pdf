1f2b873b9e2ca34d70c925f079d41e55373c73f5
peterdemartini <thepeterdemartini@gmail.com>
2018-02-25 13:19:01 -0700

    it basically works without too much breaking changes

--- command.js
+++ command.js
@@ -1,18 +1,19 @@
 #!/usr/bin/env node
 
-const map    = require('lodash/map')
-const split    = require('lodash/split')
-const includes    = require('lodash/includes')
-const first    = require('lodash/first')
-const compact    = require('lodash/compact')
-const castArray    = require('lodash/castArray')
-const path = require('path')
-const commander  = require('commander')
-const colors    = require('colors/safe')
-const HTML5ToPDF   = require('./lib')
-const { version } = require('./package.json')
+const map = require("lodash/map")
+const split = require("lodash/split")
+const includes = require("lodash/includes")
+const first = require("lodash/first")
+const compact = require("lodash/compact")
+const castArray = require("lodash/castArray")
+const toNumber = require("lodash/toNumber")
+const path = require("path")
+const commander = require("commander")
+const colors = require("colors/safe")
+const HTML5ToPDF = require("./lib")
+const { version } = require("./package.json")
 
-const printMissing = (message) => {
+const printMissing = message => {
   commander.outputHelp()
   if (message) {
     console.error(colors.red(message))
@@ -20,81 +21,74 @@ const printMissing = (message) => {
   process.exit(1)
 }
 
-const die = (error) => {
+const die = error => {
   if (!error) return process.exit(0)
-  console.error('Error:', error)
+  console.error("Error:", error)
   process.exit(1)
 }
 
 const getOptions = () => {
   commander
     .version(version)
-    .option('-i --include <path>..<path>',
-      'path to either a javascript asset, or a css asset')
-    .option('--page-size [size]',
-      "'A3', 'A4', 'Legal', 'Letter' or 'Tabloid'")
-    .option('--margin-type [n]',
-      'Specify the type of margins to use: 0 - default, 1 - none, 2 - minimum')
-    .option('--landscape',
-      'If set it will change orientation to landscape from portriat')
-    .option('--print-background',
-      'Whether to print CSS backgrounds')
-    .option('-t --template [template]',
-      'The template to used. Defaults to html5bp.')
-    .option('--template-path [/path/to/template/folder]',
-      'Specifies the template folder path for static assets, this will override template.')
-    .option('-d --render-delay [milli-seconds]',
-      'Delay before rendering the PDF (give HTML and CSS a chance to load)')
-    .option('-o --output <path>',
-      'Path of where to save the PDF')
-    .usage('[options] <path/to/html-file-path>')
+    .option(
+      "-i --include <path>..<path>",
+      "path to either a javascript asset, or a css asset",
+    )
+    .option("--page-size [size]", "'A3', 'A4', 'Legal', 'Letter' or 'Tabloid'")
+    .option(
+      "--landscape",
+      "If set it will change orientation to landscape from portriat",
+    )
+    .option("--print-background", "Whether to print CSS backgrounds")
+    .option(
+      "-t --template [template]",
+      "The template to used. Defaults to html5bp.",
+    )
+    .option(
+      "--template-path [/path/to/template/folder]",
+      "Specifies the template folder path for static assets, this will override template.",
+    )
+    .option(
+      "-d --render-delay [milli-seconds]",
+      "Delay before rendering the PDF (give HTML and CSS a chance to load)",
+    )
+    .option("-o --output <path>", "Path of where to save the PDF")
+    .usage("[options] <path/to/html-file-path>")
     .parse(process.argv)
   const inputPath = first(commander.args)
   const outputPath = commander.output
   if (inputPath == null) {
-    printMissing('Missing input path first argument')
+    printMissing("Missing input path first argument")
   }
 
-  const {
-    pageSize,
-    template,
-    templatePath,
-    renderDelay,
-    marginType
-  } = commander
+  const { pageSize, template, templatePath, renderDelay } = commander
   const printBackground = commander.printBackground
   const landscape = commander.landscape
 
-  const includeAssets = compact(castArray(split(commander.include, ',')))
-  const include = map(includeAssets, (filePath) => {
-    type = path.extname(filePath).replace('.', '')
-    if (!includes([ 'css', 'js' ], type)) {
-      die(new Error(`Invalid asset path ${filePath}`))
-    }
-    return { type, filePath }
-  })
+  const include = compact(castArray(split(commander.include, ",")))
 
   return {
     inputPath,
     outputPath,
     template,
     templatePath,
-    renderDelay,
+    renderDelay: toNumber(renderDelay),
     include,
-    pageOptions: {
+    options: {
       landscape,
       printBackground,
-      format,
-      marginType,
-    }
+      pageSize,
+    },
   }
 }
 
 const run = async () => {
   const options = getOptions()
   const pdf = new HTML5ToPDF(options)
+  await pdf.start()
   const buf = await pdf.build()
-  if(buf != null) {
+  await pdf.close()
+  if (buf != null) {
     process.stdout.write(buf)
   }
   process.exit(0)
--- examples/example.js
+++ examples/example.js
@@ -3,12 +3,18 @@ const path = require("path")
 
 const run = async () => {
   const html5ToPDF = new HTML5ToPDF({
-    inputPath: path.join(__dirname, "templates", "basic"),
+    inputPath: path.join(__dirname, "assets", "basic.html"),
     outputPath: path.join(__dirname, "..", "tmp", "output.pdf"),
+    templatePath: path.join(__dirname, "templates", "basic"),
+    include: [
+      path.join(__dirname, "assets", "basic.css"),
+      path.join(__dirname, "assets", "custom-margin.css"),
+    ],
   })
 
   await html5ToPDF.start()
   await html5ToPDF.build()
+  await html5ToPDF.close()
   console.log("DONE")
   process.exit(0)
 }
